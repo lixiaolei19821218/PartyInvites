c9f8c2dfd83578ac25ad83cdb43d7b603b4de3fa
lixiaolei19821218 <735534185@qq.com>
2018-07-19 16:36:04 +0800

    Add
    fdf

---
+++ PartyInvites/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace PartyInvites.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            int hour = DateTime.Now.Hour;
+            ViewBag.Greeting = hour < 12 ? "Good Morning" : "Good Afternoon";
+            return View("MyView");
+        }
+    }
+}
